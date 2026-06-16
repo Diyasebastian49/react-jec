6f4972d52694a028273d17fe614709a55d69f6de
Diyasebastian49 <diyasebastian49@gmail.com>
2026-06-16 15:28:40 +0530

    working on react

---
+++ react-jec/src/components/Parent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Child from './Child'
+
+const Parent= () => {
+  return (
+    <>
+    <h1>ParentComp</h1>
+    <Child name = "Kishore"dept="CSE"></Child>
+    </>
+  )
+}
+
+export default Parent
